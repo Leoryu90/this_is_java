0b44c3d5905445534ab9697a11cef1f1ec783f5f
Leoryu90 <thetein@gmail.com>
2024-05-12 23:38:31 +0900

    :memo: this_is_java day 4-2

---
+++ src/ch04/sec04/Print1to10Example.java
@@ -0,0 +1,10 @@
+package ch04.sec04;
+
+public class Print1to10Example {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i=0; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
